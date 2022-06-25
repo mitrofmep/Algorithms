aa83260258a8311b06b819ea8199fb230fa058a8
mitrofmep <58432390+mitrofmep@users.noreply.github.com>
2022-06-25 13:21:34 +0300

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -57,4 +57,30 @@ public class Solution {
         if (leftMarker < rightBorder) quickSort(myAr, leftMarker, rightBorder);
         if (leftBorder < rightMarker) quickSort(myAr, leftBorder, rightMarker);
     }
+    
+    public void heapify(int arr[], int n, int i)
+    {
+        int MAX = i; // Initialize largest as root
+        int left = 2 * i + 1; //index of the left child of ith node = 2*i + 1
+        int right = 2 * i + 2; //index of the right child of ith node  = 2*i + 2
+        int temp;
+
+        if (left < n && arr[left] > arr[MAX])            //check if the left child of the root is larger than the root
+        {
+            MAX = left;
+        }
+ 
+        if (right < n && arr[right] > arr[MAX])            //check if the right child of the root is larger than the root
+        {
+            MAX = right;
+        }
+ 
+        if (MAX != i) 
+        {                                               //repeat the procedure for finding the largest element in the heap
+            temp = arr[i];
+            arr[i] = arr[MAX];
+            arr[MAX] = temp;
+            heapify(arr, n, MAX);
+        }
+    }
 }
