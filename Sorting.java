40210229137c08c25abba09bd83ad6ce3b8f2756
mitrofmep <58432390+mitrofmep@users.noreply.github.com>
2022-06-25 13:23:55 +0300

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -58,7 +58,7 @@ public class Solution {
         if (leftBorder < rightMarker) quickSort(myAr, leftBorder, rightMarker);
     }
     
-    public void heapify(int arr[], int n, int i)
+    public static void heapify(int arr[], int n, int i)
     {
         int MAX = i; // Initialize largest as root
         int left = 2 * i + 1; //index of the left child of ith node = 2*i + 1
@@ -83,4 +83,25 @@ public class Solution {
             heapify(arr, n, MAX);
         }
     }
+    
+    public static void insertionSort(int arr[], int n) 
+    { 
+        if (n <= 1)                             //passes are done
+        {
+            return; 
+        }
+
+        insertionSort( arr, n-1 );        //one element sorted, sort the remaining array
+       
+        int last = arr[n-1];                        //last element of the array
+        int j = n-2;                                //correct index of last element of the array
+       
+        while (j >= 0 && arr[j] > last)                 //find the correct index of the last element
+        { 
+            arr[j+1] = arr[j];                          //shift section of sorted elements upwards by one element if correct index isn't found
+            j--; 
+        } 
+        arr[j+1] = last;                            //set the last element at its correct index
+    } 
+
 }
